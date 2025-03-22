e7f8582978ce0eb7f2bee99306d7d1e5c930d60d
Felippe <felippepg.goncalves@outlook.com.br>
2025-03-22 15:05:47 -0300

    feat: add new methods for Appointments

--- application/src/main/java/com/piresvet/useCaseContracts/Appointment/FindAppointmentsUseCase.java
+++ application/src/main/java/com/piresvet/useCaseContracts/Appointment/FindAppointmentsUseCase.java
@@ -3,9 +3,13 @@ package com.piresvet.useCaseContracts.Appointment;
 import com.piresvet.core.domain.Appointments;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FindAppointmentsUseCase {
     List<Appointments> findAll();
     Appointments findById(UUID id);
+
+    List<Appointments> findByCpfOwner(String cpf);
+
 }
