22293d71a2127e20a469f52ac4c6081d03f021c6
Felippe <felippepg.goncalves@outlook.com.br>
2025-03-22 15:06:09 -0300

    feat: add find methods to Appointments

--- infraestructure/src/main/java/com/piresvet/adapters/inbound/controller/AppointmentController.java
+++ infraestructure/src/main/java/com/piresvet/adapters/inbound/controller/AppointmentController.java
@@ -4,11 +4,16 @@ import com.piresvet.adapters.inbound.dtos.Appointment.AppointmentRequest;
 import com.piresvet.adapters.inbound.dtos.Appointment.AppointmentResponse;
 import com.piresvet.dataMapper.AppointmentMapper;
 import com.piresvet.useCaseContracts.Appointment.CreateAppointmentUseCase;
+import com.piresvet.useCaseContracts.Appointment.FindAppointmentsUseCase;
 import com.piresvet.useCaseContracts.Pet.FindPetsUseCase;
 import com.piresvet.useCaseContracts.Vet.FindVetUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/appointment")
@@ -20,6 +25,7 @@ public class AppointmentController {
     private final FindPetsUseCase findPetsUseCase;
     private final FindVetUseCase findVetUseCase;
     private final AppointmentMapper appointmentMapper;
+    private final FindAppointmentsUseCase findAppointmentsUseCase;
 
     @PostMapping("/create")
     public ResponseEntity<AppointmentResponse> createVet(@RequestBody AppointmentRequest request) {
@@ -30,13 +36,24 @@ public class AppointmentController {
         return ResponseEntity.ok(appointmentMapper.toResponse(response));
     }
 
-//    @GetMapping("/find-all")
-//    public List<VetResponse> getVets() {
-//        var vets = findVetUseCase.findAll();
-//        return vets.stream()
-//                .map(vet -> new VetResponse(vet.getFirstname(), vet.getLastname(), vet.getCrmv()))
-//                .collect(Collectors.toList());
-//    }
+
+    @GetMapping("/find-all")
+    public ResponseEntity<List<AppointmentResponse>> findAllAppointments() {
+        var appointments = findAppointmentsUseCase.findAll();
+        return ResponseEntity.ok(appointments.stream().map(appointmentMapper::toResponse).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<AppointmentResponse> findAppointmentById(@PathVariable("id") UUID id) {
+        var appointment = findAppointmentsUseCase.findById(id);
+        return ResponseEntity.ok(appointmentMapper.toResponse(appointment));
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity<?> findByCpf(@RequestParam(name = "cpf", required = true) String cpf) {
+        var appointments = findAppointmentsUseCase.findByCpfOwner(cpf);
+        return ResponseEntity.ok(appointments.stream().map(appointmentMapper::toResponse));
+    }
 
 
 }
--- infraestructure/src/main/java/com/piresvet/config/AppointmentConfiguration.java
+++ infraestructure/src/main/java/com/piresvet/config/AppointmentConfiguration.java
@@ -1,10 +1,13 @@
 package com.piresvet.config;
 
 import com.piresvet.gatewayContracts.Appointments.CreateAppointmentGateway;
+import com.piresvet.gatewayContracts.Appointments.FindAppointmentsGateway;
 import com.piresvet.gatewayContracts.Appointments.VerifyAvailablePetGateway;
 import com.piresvet.gatewayContracts.Appointments.VerifyAvailableVetGateway;
 import com.piresvet.useCaseContracts.Appointment.CreateAppointmentUseCase;
+import com.piresvet.useCaseContracts.Appointment.FindAppointmentsUseCase;
 import com.piresvet.useCaseImplementation.Appointment.CreateAppointmentUseCaseImpl;
+import com.piresvet.useCaseImplementation.Appointment.FindAppointmentsUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +16,10 @@ public class AppointmentConfiguration {
     @Bean
     public CreateAppointmentUseCase createAppointmentUseCase(CreateAppointmentGateway createAppointmentGateway, VerifyAvailablePetGateway verifyAvailablePetGateway, VerifyAvailableVetGateway verifyAvailableVetGateway) {
         return new CreateAppointmentUseCaseImpl(createAppointmentGateway, verifyAvailablePetGateway, verifyAvailableVetGateway);
+    }
+
+    @Bean
+    public FindAppointmentsUseCase findAppointmentsUseCase(FindAppointmentsGateway findAppointmentsGateway) {
+        return new FindAppointmentsUseCaseImpl(findAppointmentsGateway);
     }
 }
